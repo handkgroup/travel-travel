2418375ef09b2066f1d8d60467925a80cd620004
handkgroup <handkgroup@handk-inc.co.jp>
2024-07-11 14:27:53 +0500

    contact page added

--- contact_page/main.js
+++ contact_page/main.js
@@ -1,3 +1,5 @@
+// header jc
+
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const nav = document.getElementById("nav");
@@ -15,27 +17,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
   
-  // JavaScript code goes here
-  document.addEventListener('DOMContentLoaded', () => {
-      // Your JavaScript logic
-      let currentIndex = 0;
-      const slides = document.querySelectorAll('.slide');
-      const maxIndex = slides.length - 1;
-      let slideInterval;
-
-      slides[currentIndex].classList.add('visible');
-
-      function startSlide() {
-          slideInterval = setInterval(() => {
-              moveSlider();
-          }, 3000);
-      }
-
-      function moveSlider() {
-          slides[currentIndex].classList.remove('visible');
-          currentIndex = currentIndex < maxIndex ? currentIndex + 1 : 0;
-          slides[currentIndex].classList.add('visible');
-      }
-
-      startSlide();
-  });
